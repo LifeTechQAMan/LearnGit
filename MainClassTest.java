e2cd5a778e2da64fd3fe2891c42300c5d3a5b14b
LifeTechQAMan <nikolay.dikovitskij@life.com.by>
2024-07-16 19:55:06 +0300

    Введение в Java и JUnit: Тест 2

--- MainClassTest.java
+++ MainClassTest.java
@@ -3,6 +3,17 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
 
     @Test
+    public void testGetClassNumber() {
+        int b = this.getClussnumber();
+
+        if (b > 45) {
+            System.out.println("It is good");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+
+    @Test
     public void testGetLocalNumber() {
         int a = this.getLocalNumber();
 
